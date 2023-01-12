9e3f507be084dcdee61ec85b42bfda66e1835eab
Zachary Cygan <zachcygan@gmail.com>
2023-01-12 13:19:37 -0800

    removed prompt from code and replaced with it confirm for passSpecChar

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,41 +19,21 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
   var newPass = ``;
 
-  // var passLength = prompt(`Please input how long you would like your password to be. It must be between 8 and 128 characters long.`);
-  // passLength = parseInt(passLength)
+  var passLength = prompt(`Please input how long you would like your password to be. It must be between 8 and 128 characters long.`);
+  passLength = parseInt(passLength)
 
-  // while (passLength < 8 || passLength > 128) {
-  //   var passLength = prompt(`Error. Please enter a valid number. It must be between 8 and 128 characters long.`);
-  // } 
-  // confirm('Your response has been recorded.');
-  
-  // var passLowerCase = confirm(`Would you like your password to contain lowercase letters?`);
-  // confirm('Your response has been recorded.');
-  
-  // var passUpperCase = confirm(`Would you like your password to contain uppcase letters?`);
-  // confirm('Your response has been recorded.');
-  
-  // var passNumeric = confirm(`Would you like to have numeric values in your password?`);
-
-  var passSpecChar = prompt(`Please select a special character to use in your password. Available options are: !, ", #, $, %, &, ', (, ), *, @, ^, [, ], ?, -.`);
-  
-//   if (passSpecChar != `!` || passSpecChar != `"` || passSpecChar != `#` || passSpecChar != `$` 
-//                           || passSpecChar != `%` || passSpecChar != `&` || passSpecChar != "'" 
-//                           || passSpecChar != `(` || passSpecChar != `)` || passSpecChar != `*`
-//                           || passSpecChar != `@` || passSpecChar != `^` || passSpecChar != `[`
-//                           || passSpecChar != `]` || passSpecChar != `?` || passSpecChar != `-`) {
-// var passSpecChar  = prompt(`Error. Please select a valid response. Available options are: !, ", #, $, %, &, ', (, ), *, @, ^, [, ], ?, -.`);
-//   }
-
-  // if (!passSpecChar.includes("!") && !passSpecChar.includes(`"`) && !passSpecChar.includes(`#`) && !passSpecChar.includes(`#`) ||
-  //     !passSpecChar.includes(`$`) && !passSpecChar.includes(`%`) && !passSpecChar.includes(`&`) && !passSpecChar.includes(`'`) || 
-  //     !passSpecChar.includes(`(`) && !passSpecChar.includes(`)`) && !passSpecChar.includes(`*`) && !passSpecChar.includes(`@`) || 
-  //     !passSpecChar.includes(`^`) && !passSpecChar.includes(`[`) && !passSpecChar.includes(`?`) && !passSpecChar.includes(`-`)) {
-  //   passSpecChar  = prompt(`Error. Please select a valid response. Available options are: !, ", #, $, %, &, ', (, ), *, @, ^, [, ], ?, -.`);
-  // }
-
-  if (!passSpecChar.includes(specialSymbols)) {
-    passSpecChar  = prompt(`Error. Please select a valid response. Available options are: !, ", #, $, %, &, ', (, ), *, @, ^, [, ], ?, -.`);
+  while (passLength < 8 || passLength > 128) {
+    var passLength = prompt(`Error. Please enter a valid number. It must be between 8 and 128 characters long.`);
   } 
+  confirm('Your response has been recorded.');
+  
+  var passLowerCase = confirm(`Would you like your password to contain lowercase letters?`);
+  confirm('Your response has been recorded.');
+  
+  var passUpperCase = confirm(`Would you like your password to contain uppcase letters?`);
+  confirm('Your response has been recorded.');
+  
+  var passNumeric = confirm(`Would you like to have numeric values in your password?`);
 
+  var passSpecChar = confirm(`Would you like special characters in your password. Available options are: !, ", #, $, %, &, ', (, ), *, @, ^, [, ], ?, -.`);
 }
