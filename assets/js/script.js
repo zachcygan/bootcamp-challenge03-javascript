7782b073453adbd12f1f9ce927e5f2871a8abd9a
Zachary Cygan <zachcygan@gmail.com>
2023-01-12 17:32:55 -0800

    got random password to generate, but still needs work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var specialSymbols = [`!`, `"`, `#`, `$`, `%`, `&`, `'`,
-                      `(`, `)`, `*`, `@`, `^`, `[`, `]`,
-                      `?`, `-`];
+var characterLower = `abcdefghijklmnopqrstuvwxyz`
+var characterUpper = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`
+var characterNumber = `0123456789`
+var characterAll = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-?[]"`
+var characterNoSpecialChar = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`
+var characterNoNumber = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()-?[]"`
+var characterNoCaps = `abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-?[]"`
+var characterNoLower = `ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-?[]"`
 
 // Write password to the #password input
 function writePassword() {
@@ -23,7 +28,7 @@ function generatePassword() {
   passLength = parseInt(passLength)
 
   while (passLength < 8 || passLength > 128) {
-    var passLength = prompt(`Error. Please enter a valid number. It must be between 8 and 128 characters long.`);
+    passLength = prompt(`Error. Please enter a valid number. It must be between 8 and 128 characters long.`);
   } 
   confirm('Your response has been recorded.');
   
@@ -35,5 +40,26 @@ function generatePassword() {
   
   var passNumeric = confirm(`Would you like to have numeric values in your password?`);
 
-  var passSpecChar = confirm(`Would you like special characters in your password. Available options are: !, ", #, $, %, &, ', (, ), *, @, ^, [, ], ?, -.`);
+  var passSpecChar = confirm(`Would you like special characters in your password.`);
+
+  if (passLowerCase && passUpperCase && passNumeric && passSpecChar) {
+    for (var i = 0; i < passLength; i++) {
+      var randomNumber = Math.floor(Math.random()*passLength+1)
+      newPass += characterAll.charAt(randomNumber)
+    }
+
+    return newPass;
+  }
+
+  if (passLowerCase && passUpperCase && passNumeric && !passSpecChar) {
+    for (var i = 0; i < passLength; i++) {
+      var randomNumber = Math.floor(Math.random()*passLength+1)
+      newPass += characterNoSpecialChar.charAt(randomNumber)
+    }
+  }
+
+
+  // console.log(newPass)
 }
+
+
