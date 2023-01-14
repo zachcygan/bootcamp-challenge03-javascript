f095ae347c371c8a54ba596e4a7b9d5dcbf0cac3
Zachary Cygan <zachcygan@gmail.com>
2023-01-14 13:05:08 -0800

    Removed most confirmation messages and combined them into one confirmation at the end.

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,19 +27,13 @@ function generatePassword() {
   if (!(passLength >= 8 && passLength <= 128)) {
     return alert(`Error. Please enter a valid number. It must be between 8 and 128 characters long.`);
   } 
-  confirm('Your response has been recorded.');
   
   var passLowerCase = confirm(`Would you like your password to contain lowercase letters?`);
-  confirm('Your response has been recorded.');
-  
   var passUpperCase = confirm(`Would you like your password to contain uppcase letters?`);
-  confirm('Your response has been recorded.');
-  
   var passNumeric = confirm(`Would you like to have numeric values in your password?`);
-  confirm('Your response has been recorded.');
-
   var passSpecChar = confirm(`Would you like special characters in your password.`);
-  confirm('Your response has been recorded.');
+  
+  confirm('Your responses have been recorded and your password has been generated.');
 
   var stringContainer = ''
 
